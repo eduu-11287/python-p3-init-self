502232adee7715ea177dbaf47e33c110c5598abd
Edwin <edmboche141@gmail.com>
2025-02-14 13:47:05 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        return "Woof!"
+    
+fido = Dog("fido")
+fido.name
+fido.breed
